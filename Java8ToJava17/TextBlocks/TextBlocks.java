59bbb970fedfcf3401118ee4cd371fb62b832711
IssamLynx <hm_bakha@esi.dz>
2023-11-09 18:05:16 +0100

    Added TextBlocks

---
+++ Java8ToJava17/TextBlocks/TextBlocks.java
@@ -0,0 +1,15 @@
+package Java8ToJava17.TextBlocks;
+
+public class TextBlocks {
+
+    public static void main(String[] args) {
+        var name = """
+             ################################
+             ||                            ||
+             ||           Issam            ||
+             ||                            ||
+             ################################
+             """ ;
+
+    }
+}
